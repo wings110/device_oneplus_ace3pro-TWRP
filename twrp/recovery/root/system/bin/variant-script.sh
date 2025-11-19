6fe8db789e596cfbdd2ed41a5d5bb007369df00f
wings110 <283982871@qq.com>
2025-11-19 08:24:04 +0800

    Update variant-script.sh

--- twrp/recovery/root/system/bin/variant-script.sh
+++ twrp/recovery/root/system/bin/variant-script.sh
@@ -8,12 +8,12 @@ echo $variant
         case $variant in
             "22825")
             #China
-                resetprop ro.product.device "OP5929L1"
-                resetprop ro.product.vendor.device "OP5929L1"
-                resetprop ro.product.odm.device "OP5929L1"
-                resetprop ro.product.product.device "OP5929L1"
-                resetprop ro.product.device "OP5929L1"
-                resetprop ro.product.system_ext.device "OP5929L1"
+                resetprop ro.product.device "OP5D06L1"
+                resetprop ro.product.vendor.device "OP5D06L1"
+                resetprop ro.product.odm.device "OP5D06L1"
+                resetprop ro.product.product.device "OP5D06L1"
+                resetprop ro.product.device "OP5D06L1"
+                resetprop ro.product.system_ext.device "OP5D06L1"
                 resetprop ro.product.product.model "ossi"
                 resetprop ro.product.model "ossi"
                 resetprop ro.product.system.model "ossi"
@@ -29,12 +29,12 @@ echo $variant
                 resetprop ro.product.product.device "OP595DL1"
                 resetprop ro.product.device "OP595DL1"
                 resetprop ro.product.system_ext.device "OP595DL1"
-                resetprop ro.product.product.model "CPH2573"
-                resetprop ro.product.model "CPH2573"
-                resetprop ro.product.system.model "CPH2573"
-                resetprop ro.product.system_ext.model "CPH2573"
-                resetprop ro.product.vendor.model "CPH2573"
-                resetprop ro.product.odm.model "CPH2573"
+                resetprop ro.product.product.model "PJD110"
+                resetprop ro.product.model "PJD110"
+                resetprop ro.product.system.model "PJD110"
+                resetprop ro.product.system_ext.model "PJD110"
+                resetprop ro.product.vendor.model "PJD110"
+                resetprop ro.product.odm.model "PJD110"
                 resetprop ro.boot.hardware.revision "CN"
                 ;;
         esac
